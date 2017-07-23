9b6808c484cb9e1deba96405be4f1637ee9405f4
lahagemann <lahagemann@inf.ufrgs.br>
2017-07-22 22:47:10 -0300

    ???????

--- src/dropboxBackup.c
+++ src/dropboxBackup.c
@@ -27,7 +27,33 @@ void add_SSL_to_backup_socket(int socketfd)
     }
 }
 
-int main()
+int connect_server(char *host, int port)
+{
+    int socketfd;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+
+    if ( (server = gethostbyname(host)) == NULL ) 
+    {
+        fprintf(stderr,"ERROR, no such host\n");
+        exit(0);
+    }
+    
+    if ((socketfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) 
+        printf("ERROR opening socket\n");
+    
+    serv_addr.sin_family = AF_INET;     
+    serv_addr.sin_port = htons(port);    
+    serv_addr.sin_addr = *((struct in_addr *)server->h_addr);
+    bzero(&(serv_addr.sin_zero), 8);     
+     
+    if (connect(socketfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
+        printf("ERROR connecting\n");
+
+    return socketfd;
+}
+
+int main(int argc, char *argv[])
 {
 	int socketfd;
     char buffer[BUFFER_SIZE];
@@ -52,34 +78,8 @@ int main()
         abort();
     }
     
-    // conecta backup com o servidor
-    int socketfd;
-    struct sockaddr_in serv_addr;
-    struct hostent *server;
-
-	char host[512];
-	int port;
-
-	strcpy(host, argv[1]);
-	port = atoi(argv[2]);	
-
-    if ( (server = gethostbyname(host)) == NULL ) 
-    {
-        fprintf(stderr,"ERROR, no such host\n");
-        exit(0);
-    }
-    
-    if ((socketfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) 
-        printf("ERROR opening socket\n");
-    
-    serv_addr.sin_family = AF_INET;     
-    serv_addr.sin_port = htons(port);    
-    serv_addr.sin_addr = *((struct in_addr *)server->h_addr);
-    bzero(&(serv_addr.sin_zero), 8);     
-     
-    if (connect(socketfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
-        printf("ERROR connecting\n");
-
+    // conecta este cliente com o servidor, que criará uma thread para administrá-lo
+    socketfd = connect_server(argv[1], atoi(argv[2]));
     
     // adiciona SSL ao socket conectado.
     add_SSL_to_backup_socket(socketfd);
